0621cf854e6c4611b382eb21429a54715b75d97c
Sajjad <sajjaddmz89@gmail.com>
2025-12-24 21:43:37 +0330

    type fix

--- src/components/ParticlesBackground.tsx
+++ src/components/ParticlesBackground.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from 'react';
 import Particles, { initParticlesEngine } from '@tsparticles/react';
 import { loadSlim } from '@tsparticles/slim';
-import type { Container, ISourceOptions } from '@tsparticles/engine';
+import type { ISourceOptions } from '@tsparticles/engine';
 
 interface ParticlesBackgroundProps {
   className?: string;
