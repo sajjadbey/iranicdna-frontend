f8c7fbe16167b54f35efb3f4eda5f2da3561c8e4
sajjadbey <sajjaddmz89@gmail.com>
2026-01-30 17:59:24 +0330

    Insights for public

--- src/App.tsx
+++ src/App.tsx
@@ -78,9 +78,7 @@ const App: React.FC = () => {
               <Route
                 path="/admin/insights"
                 element={
-                  <ProtectedRoute>
-                    <VisitInsightsPage />
-                  </ProtectedRoute>
+                  <VisitInsightsPage />
                 }
               />
             </Routes>
